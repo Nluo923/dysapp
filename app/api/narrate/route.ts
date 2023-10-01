a5e4386ba8434102144166a09e51d1d66f6b1785
Nluo923 <38440217+Nluo923@users.noreply.github.com>
2023-09-30 18:02:44 -0700

    forced to mb gang born to sowwy

--- app/api/narrate/route.ts
+++ app/api/narrate/route.ts
@@ -35,7 +35,7 @@ export async function GET(request: Request) {
 
   return NextResponse.json({
     message: "it worked",
-    audio: `${res}`,
+    audio: `${res.stdout}`,
     success: true,
   });
 }
